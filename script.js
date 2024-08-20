0938cdde1e87c84f8ad984923a9b3aec66801adb
xFaron <harikrishnas.x@gmail.com>
2024-08-20 23:36:40 +0530

    Changes: Added face-api.js directly to code (not working)

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// DOM elements and variables
 const vidfeed = document.getElementById("vidfeed");
 const canvas = document.getElementById("vidoverlay");
 const ctx = canvas.getContext('2d');
 
+// To Stream webcam
+async function startVideo() {
+	navigator.mediaDevices.getUserMedia({ video: true })
+		.then((stream) => {
+			vidfeed.srcObject = stream; // This part sends the MediaStream to video
+		})
+		.catch((err) => {
+			console.log("Error: ", err); // To Handle vid error [TODO]
+		});
+}
+
+
+// Loads the models
+async function loadModels() {
+	await faceapi.loadTinyFaceDetectorModel('/models');
+	await faceapi.loadFaceExpressionModel('/models');
+}
 
 startVideo();
+loadModels();
+console.log(faceapi.nets)
