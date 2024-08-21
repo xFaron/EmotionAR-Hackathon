88816863a28e68a0ccbed050c1477223749a463e
xFaron <harikrishnas.x@gmail.com>
2024-08-21 14:37:20 +0530

    Added overlay canvas. Fixed face-api loading issue

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const vidfeed = document.getElementById("vidfeed");
 const canvas = document.getElementById("vidoverlay");
 const ctx = canvas.getContext('2d');
 
+let vidSize = {
+	width: vidfeed.clientWidth,
+	height: vidfeed.clientHeight
+};
+
 // To Stream webcam
 async function startVideo() {
 	navigator.mediaDevices.getUserMedia({ video: true })
 		.then((stream) => {
 			vidfeed.srcObject = stream; // This part sends the MediaStream to video
+
+			vidfeed.addEventListener("playing", () => {
+				resizeCanvas()
+			})
 		})
 		.catch((err) => {
 			console.log("Error: ", err); // To Handle vid error [TODO]
@@ -16,10 +25,44 @@ async function startVideo() {
 
 // Loads the models
 async function loadModels() {
-	await faceapi.loadTinyFaceDetectorModel('/models');
-	await faceapi.loadFaceExpressionModel('/models');
+	await faceapi.loadTinyFaceDetectorModel('./models');
+	await faceapi.loadFaceExpressionModel('./models');
 }
+
+// Detects the faces by inputting the vid stream
+async function detectFace() {
+	const detections = await faceapi.detectAllFaces(input, new faceapi.TinyFaceDetectorOptions())
+	return detections
+}
+
+// // Draws the boxes for the detections on canvas
+// function drawDetections(detections) {
+
+// }
+
+
+function resizeCanvas() {
+	vidSize = {
+		width: vidfeed.clientWidth,
+		height: vidfeed.clientHeight
+	};
+	canvas.style.width = `${vidSize.width}px`;
+	canvas.style.height = `${vidSize.height}px`;
+}
+
+
+window.addEventListener('resize', () => {
+	// When the window resizes, the canvas also has to be resized
+	resizeCanvas();
+})
 
 startVideo();
 loadModels();
-console.log(faceapi.nets)
+
+// ctx.fillStyle = 'white';
+//ctx.fillRect(0, 0, vidSize.width, vidSize.height);
+
+// setTimeout(() => {
+// 	let faces = detectFace();
+// 	drawDetections(faces);
+// }, 100);
