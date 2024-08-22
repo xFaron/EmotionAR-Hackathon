27b68da07d9b1ecf8c04dc01d521abcfe5bf55ef
xFaron <harikrishnas.x@gmail.com>
2024-08-22 15:12:02 +0530

    Recognizes emotion: prints in console

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+//DOM Elements
 const vidfeed = document.getElementById("vidfeed");
 const canvas = document.getElementById("vidoverlay");
+const camBtn = document.getElementById("camswitch");
 const ctx = canvas.getContext('2d');
+
+//Filter images
+const sunglassImg = new Image();
+sunglassImg.src = "https://w7.pngwing.com/pngs/34/292/png-transparent-sunglasses-thug-life-cool-miscellaneous-angle-white.png";
+
+
+const camBtnOffColor = 'red';
+const camBtnOnColor = 'green';
 
 let vidSize = {
 	width: vidfeed.clientWidth,
 	height: vidfeed.clientHeight
 };
+let camOn = false;
+let streamVal;
 
 // To Stream webcam
-async function startVideo() {
+function startCam() {
 	navigator.mediaDevices.getUserMedia({ video: true })
 		.then((stream) => {
+			streamVal = stream
+			camOn = true;
 			vidfeed.srcObject = stream; // This part sends the MediaStream to video
-
 			vidfeed.addEventListener("playing", () => {
 				resizeCanvas()
 			})
+			camBtn.style["background-color"] = camBtnOnColor;
 		})
 		.catch((err) => {
 			console.log("Error: ", err); // To Handle vid error [TODO]
@@ -23,11 +37,21 @@ async function startVideo() {
 }
 
 
+function stopCam() {
+	if (streamVal){
+		let tracks = streamVal.getTracks();
+		tracks.forEach((track) => {track.stop()})
+		camOn = false;
+		camBtn.style["background-color"] = camBtnOffColor;
+	}
+}
+
 // Loads the models
 async function loadModels() {
 	try {
 		console.log("Loading models");
 		await faceapi.loadTinyFaceDetectorModel('./models');
+		await faceapi.loadFaceLandmarkModel('./models')
 		await faceapi.loadFaceExpressionModel('./models');
 		console.log("Models loaded succefully!");
 		detectFaces()
@@ -39,15 +63,28 @@ async function loadModels() {
 // Detects the faces by inputting the vid stream, and draws boxes
 async function detectFaces() {
 	console.log("Detecting face");
-	const detections = await faceapi.detectAllFaces(vidfeed, new faceapi.TinyFaceDetectorOptions()); // Detect facce with TinyModel
+	const detections = await faceapi.detectAllFaces(vidfeed, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions(); // Detect facce with TinyModel
 	console.log("Detected");
 	const detectionsResized  = faceapi.resizeResults(detections, vidSize); // Resizes results for provided canvas (precaution)
 	
 	detectionsResized.forEach((face) => {
-		let x = face._box._x;
-		let y = face._box._y;
-		let width = face._box._width;
-		let height = face._box._height;
+		console.log(face);
+
+		//Reading emotion
+		let top_emotion, top_emotion_val = 0; 
+		Object.keys(face.expressions).forEach(emotion => {
+			if (top_emotion_val < face.expressions[emotion]){
+				top_emotion = emotion;
+				top_emotion_val = face.expressions[emotion];
+			}
+		})
+		console.log(top_emotion);
+
+		//Drawing bounding box
+		let x = face.detection._box._x;
+		let y = face.detection._box._y;
+		let width = face.detection._box._width;
+		let height = face.detection._box._height;
 		ctx.strokeStyle = "Blue"
 		ctx.strokeRect(x, y, width, height);
 	})
@@ -66,19 +103,33 @@ function resizeCanvas() {
 }
 
 
+function loop() {
+	if (camOn) {
+		detectFaces();
+		ctx.clearRect(0, 0, vidSize.width, vidSize.height);
+	}
+
+	requestAnimationFrame(loop);
+}
+
+
 window.addEventListener('resize', () => {
 	// When the window resizes, the canvas also has to be resized
 	resizeCanvas();
 })
 
-startVideo();
+camBtn.addEventListener('click', () => {
+	if (camOn){
+		stopCam();
+	} else {
+		startCam();
+	}
+})
+
+startCam();
 loadModels();
 
 ctx.fillStyle = 'green';
 ctx.strokeStyle = 'green';
 ctx.lineWidth = 5;
 
-setInterval(() => {
-	detectFaces();
-	ctx.clearRect(0, 0, vidSize.width, vidSize.height);
-}, 100)
